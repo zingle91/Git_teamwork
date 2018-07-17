21b73bd4ca312521f84bad3edcff109f4056a9ed
전현진 <guswls0443@naver.com>
2018-07-17 16:14:22 +0900

    레이아웃완성본

--- University/src/view/RegistView.java
+++ University/src/view/RegistView.java
@@ -17,6 +17,8 @@ import javax.swing.table.AbstractTableModel;
 
 import vo.SubjectVO;
 import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
 
 
 /*
@@ -25,17 +27,18 @@ import javax.swing.JLabel;
 public class RegistView extends JPanel {
 	JPanel west,east,west_center,west_south, west_south1,west_south2,east_north,east_north1,east_north2,
 	east_center,society,engineering,art,culture; 
-	
+	JLabel lblNewLabel,lblNewLabel_1,lblNewLabel_4,lblNewLabel_3,lblNewLabel_2,lblNewLabel_6,lblNewLabel_7,lblNewLabel_8;
+	JTextField tf0_0,tf0_1,tf0_2,tf1_0,tf1_1,tf1_2,tf2_0,tf2_1,tf2_2,tf3_0,tf3_1,tf3_2;
+
 	JTabbedPane tabbedPane;
-	
+
 	JButton society_btn1,society_btn2,engineering_btn1,engineering_btn2,art_btn1,art_btn2,culture_btn1,culture_btn2,
 	confirmBtn,scoreSearchBtn,logoutBtn,plusbtn;
-	
-	JTable avaiSubTable,registTable;
+
+	JTable avaiSubTable;
 
 	avaiSubModel avaiModel;
 	registListModel registModel;
-
 
 	public RegistView() {
 		setLayout(new GridLayout(1, 2, 0, 0)); //전체 1행2열(왼쪽,오른쪽영역)
@@ -46,10 +49,8 @@ public class RegistView extends JPanel {
 
 		west_center = new JPanel();
 		west.add(west_center, BorderLayout.CENTER);
-		
+
 		registModel=new registListModel();
-		registTable = new JTable(registModel);
-		west_center.add(new JScrollPane(registTable));
 
 		west_south = new JPanel();
 		west_south.setBorder(new TitledBorder(null, "\uC774\uC218\uD559\uC810", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -58,10 +59,79 @@ public class RegistView extends JPanel {
 
 		west_south1 = new JPanel();
 		west_south.add(west_south1);
+		west_south1.setLayout(new GridLayout(5, 4, 0, 0));
 
-		JTextArea textArea = new JTextArea();
-		textArea.setText("이수학점 들어가는 부분!! ");
-		west_south1.add(textArea);
+		lblNewLabel = new JLabel("구분");
+		west_south1.add(lblNewLabel);
+
+		lblNewLabel_1 = new JLabel("전체");
+		west_south1.add(lblNewLabel_1);
+
+		lblNewLabel_4 = new JLabel("이수");
+		west_south1.add(lblNewLabel_4);
+
+		lblNewLabel_3 = new JLabel("남은학점");
+		west_south1.add(lblNewLabel_3);
+
+		lblNewLabel_6 = new JLabel("종합");
+		west_south1.add(lblNewLabel_6);
+
+		tf0_0 = new JTextField();
+		west_south1.add(tf0_0);
+		tf0_0.setColumns(10);
+
+		tf0_1 = new JTextField();
+		west_south1.add(tf0_1);
+		tf0_1.setColumns(10);
+
+		tf0_2 = new JTextField();
+		west_south1.add(tf0_2);
+		tf0_2.setColumns(10);
+
+		lblNewLabel_2 = new JLabel("전공");
+		west_south1.add(lblNewLabel_2);
+
+		tf1_0 = new JTextField();
+		west_south1.add(tf1_0);
+		tf1_0.setColumns(10);
+
+		tf1_1 = new JTextField();
+		west_south1.add(tf1_1);
+		tf1_1.setColumns(10);
+
+		tf1_2 = new JTextField();
+		west_south1.add(tf1_2);
+		tf1_2.setColumns(10);
+
+		lblNewLabel_7 = new JLabel("영어");
+		west_south1.add(lblNewLabel_7);
+
+		tf2_0 = new JTextField();
+		west_south1.add(tf2_0);
+		tf2_0.setColumns(10);
+
+		tf2_1 = new JTextField();
+		west_south1.add(tf2_1);
+		tf2_1.setColumns(10);
+
+		tf2_2 = new JTextField();
+		west_south1.add(tf2_2);
+		tf2_2.setColumns(10);
+
+		lblNewLabel_8 = new JLabel("교양");
+		west_south1.add(lblNewLabel_8);
+
+		tf3_0 = new JTextField();
+		west_south1.add(tf3_0);
+		tf3_0.setColumns(10);
+
+		tf3_1 = new JTextField();
+		west_south1.add(tf3_1);
+		tf3_1.setColumns(10);
+
+		tf3_2 = new JTextField();
+		west_south1.add(tf3_2);
+		tf3_2.setColumns(10);
 
 		west_south2 = new JPanel();
 		west_south.add(west_south2);
@@ -125,7 +195,7 @@ public class RegistView extends JPanel {
 		east_center.setToolTipText("");
 		east_center.setBorder(new TitledBorder(null, "\uC2E0\uCCAD\uAC00\uB2A5\uACFC\uBAA9", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		east.add(east_center, BorderLayout.CENTER);
-		
+
 		avaiModel=new avaiSubModel();
 		avaiSubTable = new JTable(avaiModel);
 		east_center.add(new JScrollPane(avaiSubTable));
@@ -148,8 +218,8 @@ public class RegistView extends JPanel {
 	class ButtonEventHandler implements ActionListener{
 		public void actionPerformed(ActionEvent ev){
 			Object o = ev.getSource();
-		//	SubjectVO sub=new SubjectVO();
-			
+			//	SubjectVO sub=new SubjectVO();
+
 			if(o==society_btn1){  
 				//avaiModel.data=data;
 				avaiSubTable.setModel(avaiModel);
@@ -195,7 +265,7 @@ public class RegistView extends JPanel {
 			}
 		}
 	}
-	
+
 	//테이블 모델
 	class avaiSubModel extends AbstractTableModel{
 		String[] colName = {"학수번호","강좌명","강의시간","정원","교수명","학점","신청"};
@@ -211,7 +281,7 @@ public class RegistView extends JPanel {
 			ArrayList temp=(ArrayList)data.get(row);
 			return temp.get(col);
 		}//End 필수오버라이딩
-		
+
 		//내가설정한 이름으로 속성이름 설정하기
 		public String getColumnName(int col) {
 			return colName[col];
@@ -231,7 +301,7 @@ public class RegistView extends JPanel {
 			ArrayList temp=(ArrayList)data.get(row);
 			return temp.get(col);
 		}//End 필수오버라이딩
-		
+
 		//내가설정한 이름으로 속성이름 설정하기
 		public String getColumnName(int col) {
 			return colName[col];
