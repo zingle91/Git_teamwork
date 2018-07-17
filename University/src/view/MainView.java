2d1394c3138da72422d39f4bfb5af3ce2e07d449
전현진 <guswls0443@naver.com>
2018-07-17 10:03:21 +0900

    장호오빠꺼 그대로

--- University/src/view/MainView.java
+++ University/src/view/MainView.java
@@ -9,20 +9,30 @@ import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.FlowLayout;
 import javax.swing.JInternalFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.border.TitledBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainView extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField tfStdnum;
+	
+	JButton login;
 
 
+	/**
+	 * Create the frame.
+	 */
 	public MainView() {
+		
+		// 화면설계
 		setResizable(false);
 		setTitle("University");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,12 +46,12 @@ public class MainView extends JFrame {
 		panel.setBorder(new TitledBorder(null, "\uD559\uC0DD", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		contentPane.add(panel);
 		
-		textField = new JTextField();
-		panel.add(textField);
-		textField.setColumns(10);
+		tfStdnum = new JTextField();
+		panel.add(tfStdnum);
+		tfStdnum.setColumns(10);
 		
-		JButton button = new JButton("로그인");
-		panel.add(button);
+		login = new JButton("로그인");
+		panel.add(login);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "\uAD00\uB9AC\uC790", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -49,6 +59,41 @@ public class MainView extends JFrame {
 		
 		JButton btnNewButton = new JButton("관리자로 로그인");
 		panel_1.add(btnNewButton);
+		
+		
+		
+		// 이벤트
+		eventProc();
 	}
+
+	public void semiclose() {
+		setVisible(false);
+	}
+	
+	
+	public void eventProc() {
+		EvtHdlr hdlr = new EvtHdlr();
+		login.addActionListener(hdlr);
+	}
+	
+	
+	class EvtHdlr implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object btn = e.getSource();
+			if(btn == login) {
+				String sNum = null;
+				sNum = tfStdnum.getText();
+				StdView frame = new StdView(sNum);
+				frame.setVisible(true);
+				semiclose();
+				JOptionPane.showMessageDialog(null,"입력한 학번 : " + sNum);
+			}
+			
+		}
+		
+	}
+	
 
 }
